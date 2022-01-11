a55f0a33affa5975d03b95d2bd0e82b39cf51fd0
João Pedro F. Dantas <fonsecajope@gmail.com>
2022-01-11 10:57:07 -0300

    Create homepage with aside element

---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import illustrationImg from '../assets/images/illustration.svg'
+
+export function Home() {
+  return (
+    <div>
+      <aside>
+        <img src="{illustrationImg}" alt="Ilustração que simboliza perguntas e respostas" />
+        <strong>Crie sala de Q&amp;A ao vivo</strong>
+        <p>Tire as dúvidas da sua audiência em tempo real</p>
+      </aside>
+    </div>
+  )
+}
